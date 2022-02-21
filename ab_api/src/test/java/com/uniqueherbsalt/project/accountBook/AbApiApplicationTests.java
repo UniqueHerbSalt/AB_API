c872bf24d9a68bfdb3d7953c94b3bb3700337f18
UniqueHerbSalt <UniqueHerbSalt@gmail.com>
2022-02-21 23:12:35 +0900

    Init

---
+++ ab_api/src/test/java/com/uniqueherbsalt/project/accountBook/AbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniqueherbsalt.project.accountBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
